4f39847fb4b52de2e7eb842f5c31d45de61868be
ZK <kongzhikeng@gmail.com>
2023-03-14 10:05:22 -0400

    handle function and props

--- weather-radar/src/App.js
+++ weather-radar/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  const cityList = [
+    {
+      id: 1,
+      name: "Boston",
+      zipCode: "02111",
+      country: {
+        id: "US",
+        name: "United States"
+      },
+      admin: {
+        id: "MA",
+        name: "Massachusetts",
+        type: "State"
+      }
+    },
+    {      
+      id: 2,
+      name: "Los Angeles",
+      zipCode: "90048",
+      country: {
+        id: "US",
+        name: "United States"
+      },
+      admin: {
+        id: "CA",
+        name: "California",
+        type: "State"
+      }
+    },
+    {      
+      id: 3,
+      name: "New York City",
+      zipCode: "10001",
+      country: {
+        id: "US",
+        name: "United States"
+      },
+      admin: {
+        id: "NY",
+        name: "New York",
+        type: "State"
+      }
+    }
+   ];
+
+   const handleDisplayInput = (e) => {
+    console.log(e.target.value)
+   }
+
+   const CityDisplay = (props) => {
+    return(
+    props.list.map(
+      cityList=> (
+      <ul key={cityList.id}>
+        <li>{cityList.name}, {cityList.admin.id}, {cityList.country.id}</li>
+      </ul>
+    )
+    )
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='inputDisplay'>
+      <h1>Weather Radar</h1>
+      <label htmlFor='city-name'>city: </label>
+      <input id='city-name' name='city-name' type='text' onChange={handleDisplayInput}></input>
+      <hr />
+      <CityDisplay list = {cityList} />
     </div>
-  );
+  )
 }
 
 export default App;
